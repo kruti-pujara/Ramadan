9bc68c8ccc346a1aaa420378e25fb996db069738
Kruti <krutipujara17@gmail.com>
2022-03-21 08:45:34 +0530

    Ramadan assignment done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
     jQuery(window).on("scroll", function(){
-        if($(window).scrollTop()){
+        if(jQuery(window).scrollTop()){
             jQuery('.site-header').addClass('blue');
         }
         else{
@@ -11,5 +11,31 @@ jQuery(document).ready(function(){
         jQuery('html, body').toggleClass('open-menu')
         jQuery('.hamburger').toggleClass('open-menu')
         jQuery('.navbar').toggleClass('open-menu')
+    });
+    jQuery("#purple").click(function(){
+        jQuery("#ramadan-content").text("you have clicked the purple button.. here is its content")
+        jQuery("#ramadan-heading").text("TasteRamadan 2017 ")
+        jQuery(".taste-ramadan-content").css("background-color", "#bc498f")
+        
+    });
+    jQuery("#blue").click(function(){
+        jQuery("#ramadan-content").text("you have clicked the Blue button.. here is its content")
+        jQuery("#ramadan-heading").text("TasteRamadan 2018 ")
+        jQuery(".taste-ramadan-content").css("background-color", "#718cb5")
+        // jQuery(".taste-ramadan-content").before(jQuery(".taste-ramadan-content").css({"background-color":"#718cb5"}));
+        
+    });
+    jQuery("#yellow").click(function(){
+        jQuery("#ramadan-content").text("you have clicked the Yellow button.. here is its content")
+        jQuery("#ramadan-heading").text("TasteRamadan 2019 ")
+        jQuery(".taste-ramadan-content").css("background-color", "#e1a12d")
+        // jQuery(".taste-ramadan-content").addClass("yellow-click")
+        
+    });
+    jQuery("#green").click(function(){
+        jQuery("#ramadan-content").text("you have clicked the Green button.. here is its content")
+        jQuery("#ramadan-heading").text("TasteRamadan 2020 ")
+        jQuery(".taste-ramadan-content").css("background-color", "#0f9e8d")
+        
     });
 });
